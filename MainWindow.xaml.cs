e9acf581f7605feacdf0dd98cf308cfdb1da6b68
yermak <yermak@asuinc.ru>
2021-11-30 17:54:09 +1000

    Остался косяк с размером окна

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Сache_Сleaner
 {
     /// <summary>
@@ -20,9 +22,80 @@ namespace Сache_Сleaner
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+            TreeViewItem treeViewItem = new TreeViewItem();
+            cElement element = new cElement("");
+            //<TreeView x:Name="treeViewBases"  Margin="10,0,10,10" />
+            string fileName = Environment.GetEnvironmentVariable("AppData")+ "\\1C\\1CEStart\\ibases.v8i";
+            foreach (string line in System.IO.File.ReadLines(fileName))
+            {
+                if (line.StartsWith("[") && line.EndsWith("]"))
+                {
+                    element = new cElement(line.Substring(1, line.Length - 2));
+                    treeViewItem = new TreeViewItem();
+                    treeViewItem.Header = element.ToString();
+                    treeViewItem.Tag = element;
+                }
+
+                if (line.StartsWith("ID="))                   
+                    element.guid = new Guid(line.Substring(3));
+
+                if (line.StartsWith("Connect="))
+                    element.Connect = line.Substring(8);
+                    
+                if (line.StartsWith("Folder="))
+                {
+
+                    element.Folder = line.Substring(7);                    
+                    
+                    if (element.Folder == @"/")
+                        treeViewBases.Items.Add(treeViewItem);
+                    else
+                    {
+                        ItemCollection ic = treeViewBases.Items;
+                        foreach (string path in element.Folder.Split('/'))
+                        {
+                            foreach (TreeViewItem item in ic)
+                                if (((cElement)item.Tag).Name.Equals(path))
+                                {
+                                    ic = item.Items;
+                                    break;
+                                }
+                        }
+                        ic.Add(treeViewItem);
+                    }
+                    
+                }
+
+            }
+
+            
+               
+        }
+
+        private void ShowInfo()
+        {
+            cElement element = ((cElement)((TreeViewItem)treeViewBases.SelectedItem).Tag);
+            LabelPath.Content = element.Connect;
+            TextGuid.Text = "GUID = " + element.guid.ToString() + "; Size = " + (element.CacheSize/1024/1024).ToString("# ##0") + " mb";
+        }
+
+        private void treeViewBases_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            ShowInfo();
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ((cElement)((TreeViewItem)treeViewBases.SelectedItem).Tag).ClearCache();
+            ShowInfo();
+
+
         }
     }
 }
---
+++ cElement.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Сache_Сleaner
+{
+    class cElement
+    {
+        public string Name { set; get; }
+        public Guid guid { set; get; }
+        public string Folder { set; get; }
+        public bool isFolder { set; get; }
+        public string Connect { set; get; }
+        public List<string> Pathes
+        { 
+            get 
+            {
+                List<string> p = new List<string>();
+                p.Add(Environment.GetEnvironmentVariable("AppData") + "\\1C\\1cv8\\" + guid.ToString());
+                p.Add(Environment.GetEnvironmentVariable("AppData") + "\\..\\Local\\1C\\1cv8\\" + guid.ToString());
+                return p;
+            }
+        }
+
+        public long CacheSize
+        {
+            
+            get
+            {
+                long size =  0;
+                foreach(string Path in Pathes)
+                    size += SafeEnumerateFiles(Path, "*.*", SearchOption.AllDirectories).Sum(n => new FileInfo(n).Length);
+                return size;
+            }
+        }
+
+        private static IEnumerable<string> SafeEnumerateFiles(string path, string searchPattern = "*.*", SearchOption searchOption = SearchOption.TopDirectoryOnly)
+        {
+            var dirs = new Stack<string>();
+            dirs.Push(path);
+
+            while (dirs.Count > 0)
+            {
+                string currentDirPath = dirs.Pop();
+                if (searchOption == SearchOption.AllDirectories)
+                {
+                    try
+                    {
+                        string[] subDirs = Directory.GetDirectories(currentDirPath);
+                        foreach (string subDirPath in subDirs)
+                        {
+                            dirs.Push(subDirPath);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        continue;
+                    }
+                    catch (DirectoryNotFoundException)
+                    {
+                        continue;
+                    }
+                }
+
+                string[] files = null;
+                try
+                {
+                    files = Directory.GetFiles(currentDirPath, searchPattern);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    continue;
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    continue;
+                }
+
+                foreach (string filePath in files)
+                {
+                    yield return filePath;
+                }
+            }
+        }
+
+        public cElement(string name)
+        {
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+
+
+
+        public void ClearCache()
+
+        {
+            foreach (string path in Pathes)
+                Directory.Delete(path, true);
+        }
+
+    }
+}
